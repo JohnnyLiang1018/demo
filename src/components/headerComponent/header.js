b47f6f722e0d9167956336fc1e92eb52da718cc8
Johnny Liang <zhonglin.liang@sjsu.edu>
2019-02-25 11:44:17 -0800

    2/25 update

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className="logo">
+            LOGO
+          </div>
+        </header>
+      </div>
+    );
+  }
+
+}
+
+export default Header;
---
+++ src/components/pages/homePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+
+  render() {
+    return (
+      <div className="wrap">
+        Home Page Content 
+      </div>
+    );
+  }
+
+}
+
+export default Homepage;
