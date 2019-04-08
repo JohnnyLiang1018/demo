a10fc15fef9c9ec8207f6f3ed4bdb142f9d31709
Johnny Liang <zhonglin.liang@sjsu.edu>
2019-04-08 11:11:47 -0700

    react component update

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  componentWillMount(){
+    console.log('Component WILL MOUNT!')
+  }
+  componentDidMount(){
+    console.log('Component DID MOUNT!')
+  }
+  componentWillReceiveProps(newProps){
+    console.log('Component WILL RECEIVE PROPS!')
+  }
+  shouldComponentUpdate(newProps, newState) {
+    return true;
+ }
+ componentWillUpdate(nextProps, nextState) {
+    console.log('Component WILL UPDATE!');
+ }
+ componentDidUpdate(prevProps, prevState) {
+    console.log('Component DID UPDATE!')
+ }
+ componentWillUnmount() {
+    console.log('Component WILL UNMOUNT!')
+ }
 
   render() {
     return (
-      <div className="App">
-        <header>
-          <div className="logo">
-            LOGO
-          </div>
-        </header>
+      <div>
+        <h3> {this.props.header}</h3>
       </div>
     );
   }
