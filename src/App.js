99b274ea595ef4069e692aae1f55ba93aaec80a3
Johnny Liang <zhonglin.liang@sjsu.edu>
2019-04-15 14:05:56 -0700

    index page react update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import * as ReactDOM from 'react-dom';
 import Footer from './components/footerComponent/footer'
 import Homepage from './components/pages/homePage'
 import Menu from './components/Menu/menu'
 import './css/individual_style.css'
 import FormComponent from './components/FormComponent/FormComponet'
+require('./vendors/css/ionicons.min.css')
+require('./vendors/css/normalize.css')
+require('./vendors/css/grid.css')
+require('./resources/css/style.css')
+require('./resources/css/queries.css')
+require('./resources/css/font.css')
 
 class App extends Component {
   constructor(props){
@@ -11,6 +18,7 @@ class App extends Component {
 
     this.state = {
       welcomeMessage: 'Welcome'
+
       
     }
     this.setwelcomeMessage = this.setwelcomeMessage.bind(this)
@@ -28,21 +36,21 @@ class App extends Component {
     if(this.state.welcomeMessage === 'Welcome'){
 
     return (
+      <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <title>ThunderBolt</title>
+      </Head>
+
       <body>
         
         <div className="App">
           <Header welcomeMessage = {this.state.welcomeMessage}> </Header>
-          
-          <Section_one />
-
-          <Section_two />
-
-          <Section_three />
-
-          <Footer />
-          <button onClick = {this.setwelcomeMessage}>changeHeader</button>
+          <Section_one></Section_one>
+          <Section_two></Section_two>
         </div>
       </body>
+      </div>
     );
     }
     else{
@@ -61,6 +69,12 @@ class App extends Component {
   }
 
 }
+class Head extends React.Component {
+  render(){
+    return ReactDOM.createPortal(this.props.children,document.head);
+  }
+}
+
 
 class Header extends React.Component {
   componentWillMount(){
@@ -88,17 +102,22 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <div class="container">
-            <div id="branding">
-             
-            </div>
-            <nav>
-                <ul>
-                    <li class="current"> <a href="index.html">{this.props.welcomeMessage}</a></li>
-                    
-                </ul>
-            </nav>
-        
+        <nav>
+          <div class="row">
+            <img src={require('./resources/img/logo.png')} alt="logo" class="logo-black"></img>
+            <ul class ="main-nav">
+              <li><a href="#">My Purchases</a></li>
+              <li><a href="#">Aisles</a></li>
+              <li><a href="#">Deals</a></li>
+              <li><a href="#">Recipes</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div class="hero-text-box">
+          <h1>Market fresh.</h1>
+          <h1>Money smart.</h1>
+          <a class="btn btn-full" href="#">Shop Now</a>
+          <a class="btn btn-ghost" href="#">About Us</a>
         </div>
       </div>
     );
@@ -109,21 +128,24 @@ class Header extends React.Component {
 class Section_one extends React.Component{
   render(){
       return(
-        <section id = "showcase">
-        <div id = "container">
-            <div id="branding">
-            <h1>THUNDERBOLTS</h1>
-            <nav>
-                <ul>
-                    <li class="current"><a href="index.html">My Purchases</a></li>
-                    <li class="current"><a href="index.html">Aisles</a></li>
-                    <li class="current"><a href="index.html">Deals</a></li>
-                    <li class="current"><a href="index.html">Reciepes</a></li>
-                    
-                </ul>
-            </nav>
+        <section class="section-categories">
+          <div class="row">
+            <h2>Main Categories</h2>
+          </div>
+          <div class="row">
+            <div class="col span-1-of-4">
+              <h3>Baby Care</h3>
             </div>
-        </div>
+            <div class="col span-1-of-4">
+              <h3>Beverages</h3>
+            </div>
+            <div class="col span-1-of-4">
+              <h3>Bread &amp; Bakery</h3>
+            </div>
+            <div class="col span-1-of-4">
+              <h3>Breakfast &amp; Cereal</h3>
+            </div>
+          </div>
         </section>
         );
     }
@@ -155,23 +177,51 @@ class Section_one_v2 extends React.Component{
 class Section_two extends React.Component{
   render(){
       return(
-        <section id = "search">
-          <div class="container">
-            <nav>
-
-              <form>
-                <input type="text" placeholder="Search Products"></input>
-                <button type="submit" class="addButton buttonStyle">Enter</button>
-
-              </form>
-
-              <div>
-                <a href="index.html"><i class="fa fa-angle-double-left"> Back </i></a>
-
-              </div>
-
-            </nav>
-          </div>
+        <section class="section-pics">
+          <ul class="pics-showcase clearfix">
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/1.jpg')} alt="Food 1"></img>
+              </figure>
+            </li>
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/2.jpg')} alt="Food 2"></img>
+              </figure>
+            </li>
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/3.jpg')} alt="Food 3"></img>
+              </figure>
+            </li>
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/4.jpg')} alt="Food 4"></img>
+              </figure>
+            </li>
+          </ul>
+          <ul class="pics-showcase clearfix">
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/5.jpg')} alt="Food 5"></img>
+              </figure>
+            </li>
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/6.jpg')} alt="Food 6"></img>
+              </figure>
+            </li>
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/7.jpg')} alt="Food 7"></img>
+              </figure>
+            </li>
+            <li>
+              <figure class="pic-photo">
+                <img src={require('./resources/img/8.jpg')} alt="Food 8"></img>
+              </figure>
+            </li>
+          </ul>
         </section>
       );
     }
